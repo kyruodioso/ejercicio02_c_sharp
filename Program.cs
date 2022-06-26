92e1a99c9fd443beb89db72922a6ec8eb9d740e3
kyru odioso <kanu_nirvana@hotmail.com>
2022-06-26 12:53:02 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿/*Ejercicio 2
+
+Escribe un programa que tome la hora y la escriba por pantalla.*/
+
+string horaActual= DateTime.Now.ToString("hh:mm");
+
+Console.WriteLine("La hora actual es " + horaActual);
